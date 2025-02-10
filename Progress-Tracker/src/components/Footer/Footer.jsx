9f9be8d629d5af10af89df1fffc32206d055d89c
Akash Kumar Pathak <akashp1353@gmail.com>
2025-02-10 23:52:09 +0530

    modified footer

--- Progress-Tracker/src/components/Footer/Footer.jsx
+++ Progress-Tracker/src/components/Footer/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 function Footer(){
     return(
-        <div>Footer</div>
+        <div>Footer, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex laboriosam blanditiis dolor omnis reprehenderit qui nam fuga aliquid eum voluptate?</div>
     )
 }
