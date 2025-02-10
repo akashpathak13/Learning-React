bf37fffebd6db8edcd11ed10e6e5313d945f6013
Akash Kumar Pathak <akashp1353@gmail.com>
2025-02-10 23:53:06 +0530

    modified navbar

--- Progress-Tracker/src/components/Header/Navbar.jsx
+++ Progress-Tracker/src/components/Header/Navbar.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 function Navbar(){
     return(
-        <div>Navbar</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat consectetur repellat odit provident non?</div>
     )
 }
