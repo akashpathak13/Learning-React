94227b3c7692203fe7d062e696358a13c910ff23
Akash Kumar Pathak <akashp1353@gmail.com>
2025-02-13 23:21:59 +0530

    debug 2

--- Blog Project/src/store/authSlice.js
+++ Blog Project/src/store/authSlice.js
@@ -10,7 +10,7 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         login: (state, action) => {
-            state.status = truestate
+            state.status = true
             state,userData = action.payload.userData
         },
         logout: (state) => {
