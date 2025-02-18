33885fe6db435859b780464668a923c06acfa30a
Akash Kumar Pathak <akashp1353@gmail.com>
2025-02-18 22:06:58 +0530

    context switching

--- Practise/function.js
+++ Practise/function.js
@@ -13,3 +13,84 @@ ans.map((curr) => {
 for(let i=0; i<=10; i++){
     console.log(i*10)
 }
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+ans = list.filter((curr) => (curr%2 !== 0))
+ans.map((curr) => {
+    // console.log(curr)
+})
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+ans = list.filter((curr) => (curr%2 !== 0))
+ans.map((curr) => {
+    // console.log(curr)
+})
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+ans = list.filter((curr) => (curr%2 !== 0))
+ans.map((curr) => {
+    // console.log(curr)
+})
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+ans = list.filter((curr) => (curr%2 !== 0))
+ans.map((curr) => {
+    // console.log(curr)
+})
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i*10)
+}
