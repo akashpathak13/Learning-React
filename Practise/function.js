f29646b2acce87717d8054077ba2a48fdf10025f
Akash Kumar Pathak <akashp1353@gmail.com>
2025-02-19 23:33:58 +0530

    practise js

--- Practise/function.js
+++ Practise/function.js
@@ -5,492 +5,160 @@ list.map((curr) => {
     // console.log(curr)
 })
 
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
-console.log('Hello world')
-list = [1,2,3,4,5,6,7,8]
-// console.log(ans)
-list.map((curr) => {
-    // console.log(curr)
-})
-
-ans = list.filter((curr) => (curr%2 !== 0))
-ans.map((curr) => {
-    // console.log(curr)s
-})
-
-for(let i=0; i<=10; i++){
-    console.log(i*10)
-}
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
+console.log('this is fun')
