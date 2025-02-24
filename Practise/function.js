904e826a5761e4b4401559f6b86d896cdb088bed
Akash Kumar Pathak <akashp1353@gmail.com>
2025-02-24 23:52:20 +0530

    dsa

--- Practise/function.js
+++ Practise/function.js
@@ -168,6 +168,350 @@ list = [1,2,3,4,5,6,7,8]
 list.map((curr) => {
     // console.log(curr)
 })
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+console.log('Hello world')
+list = [1,2,3,4,5,6,7,8]
+// console.log(ans)
+list.map((curr) => {
+    // console.log(curr)
+})
+
 console.log('Hello world')
 list = [1,2,3,4,5,6,7,8]
 // console.log(ans)
